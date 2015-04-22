5ad1df50427e4c41bbaddd5c23b518175b3f49bc
Michael Parshin <parshin@outlook.com>
2015-04-22 14:27:36 +0300

    SignalR integration test

---
+++ Sources/Core.SignalR.IntegrationTests/SignalRBackplaneTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.SignalR;
+using NUnit.Framework;
+
+namespace Core.SignalR.IntegrationTests
+{
+    [TestFixture]
+    public class SignalRBackplaneTest
+    {
+        [Test]
+        public void ConectToSignalR_Send_ReceiveMessage()
+        {
+            GlobalHost.DependencyResolver.UseRabbit(new RabbitScaleoutConfiguration
+            {
+                ConnectionString = "amqp://localhost"
+            });
+
+            GlobalHost.ConnectionManager.GetHubContext<TestHub>().Clients.All.updateData("Hello, World!");
+        }
+    }
+
+    public class TestHub : Hub
+    {
+    }
+}
