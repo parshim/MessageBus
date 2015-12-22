e855eef809facf46844e42e00de1a58d03dd430f
Michael <parshim@gmail.com>
2015-12-22 09:49:37 +0200

    Hierarchy subscription

--- Sources/Core.Proxy/ISubscriptionSelector.cs
+++ Sources/Core.Proxy/ISubscriptionSelector.cs
@@ -7,12 +7,12 @@ namespace MessageBus.Core.Proxy
 {
     public interface ISubscriptionSelector<TContract> : IDisposable
     {
-        void Subscribe<TData>(Expression<Func<TContract, Action<TData>>> methodSelector, Action<TData> notificationCallback, params BusHeader[] filterHeaders);
-        
-        void Subscribe(Expression<Func<TContract, Action>> methodSelector, Action notificationCallback, params BusHeader[] filterHeaders);
+        void Subscribe<TData>(Expression<Func<TContract, Action<TData>>> methodSelector, Action<TData> notificationCallback, bool hierarchy = false, params BusHeader[] filterHeaders);
 
-        void Subscribe<TData>(Expression<Func<TContract, Action<TData>>> methodSelector, Action<TData, IEnumerable<BusHeader>> notificationCallback, params BusHeader[] filterHeaders);
+        void Subscribe(Expression<Func<TContract, Action>> methodSelector, Action notificationCallback, bool hierarchy = false, params BusHeader[] filterHeaders);
 
-        void Subscribe(Expression<Func<TContract, Action>> methodSelector, Action<IEnumerable<BusHeader>> notificationCallback, params BusHeader[] filterHeaders);
+        void Subscribe<TData>(Expression<Func<TContract, Action<TData>>> methodSelector, Action<TData, IEnumerable<BusHeader>> notificationCallback, bool hierarchy = false, params BusHeader[] filterHeaders);
+
+        void Subscribe(Expression<Func<TContract, Action>> methodSelector, Action<IEnumerable<BusHeader>> notificationCallback, bool hierarchy = false, params BusHeader[] filterHeaders);
     }
 }
--- Sources/Core.Proxy/SubscriptionSelector.cs
+++ Sources/Core.Proxy/SubscriptionSelector.cs
@@ -18,7 +18,7 @@ namespace MessageBus.Core.Proxy
             _messageFactory = messageFactory;
         }
 
-        public void Subscribe<TData>(Expression<Func<T, Action<TData>>> methodSelector, Action<TData> notificationCallback, params BusHeader[] filterHeaders)
+        public void Subscribe<TData>(Expression<Func<T, Action<TData>>> methodSelector, Action<TData> notificationCallback, bool hierarchy = false, params BusHeader[] filterHeaders)
         {
             MethodInfo methodInfo = GetMethodInfo(methodSelector);
 
@@ -39,10 +39,10 @@ namespace MessageBus.Core.Proxy
 
                 notificationCallback((TData) value);
 
-            }, filter: filterHeaders);
+            }, filter: filterHeaders, hierarchy: hierarchy);
         }
 
-        public void Subscribe(Expression<Func<T, Action>> methodSelector, Action notificationCallback, params BusHeader[] filterHeaders)
+        public void Subscribe(Expression<Func<T, Action>> methodSelector, Action notificationCallback, bool hierarchy = false, params BusHeader[] filterHeaders)
         {
             MethodInfo methodInfo = GetMethodInfo(methodSelector);
 
@@ -55,10 +55,10 @@ namespace MessageBus.Core.Proxy
 
             Type messageType = _messageFactory.GetMessageType(methodInfo);
 
-            _subscriber.Subscribe(messageType, (object o) => notificationCallback(), filter: filterHeaders);
+            _subscriber.Subscribe(messageType, (object o) => notificationCallback(), filter: filterHeaders, hierarchy: hierarchy);
         }
 
-        public void Subscribe<TData>(Expression<Func<T, Action<TData>>> methodSelector, Action<TData, IEnumerable<BusHeader>> notificationCallback, params BusHeader[] filterHeaders)
+        public void Subscribe<TData>(Expression<Func<T, Action<TData>>> methodSelector, Action<TData, IEnumerable<BusHeader>> notificationCallback, bool hierarchy = false, params BusHeader[] filterHeaders)
         {
             MethodInfo methodInfo = GetMethodInfo(methodSelector);
 
@@ -79,10 +79,10 @@ namespace MessageBus.Core.Proxy
 
                 notificationCallback((TData)value, m.Headers.OfType<BusHeader>());
 
-            }, filter: filterHeaders);
+            }, filter: filterHeaders, hierarchy: hierarchy);
         }
 
-        public void Subscribe(Expression<Func<T, Action>> methodSelector, Action<IEnumerable<BusHeader>> notificationCallback, params BusHeader[] filterHeaders)
+        public void Subscribe(Expression<Func<T, Action>> methodSelector, Action<IEnumerable<BusHeader>> notificationCallback, bool hierarchy = false, params BusHeader[] filterHeaders)
         {
             MethodInfo methodInfo = GetMethodInfo(methodSelector);
 
@@ -95,7 +95,7 @@ namespace MessageBus.Core.Proxy
 
             Type messageType = _messageFactory.GetMessageType(methodInfo);
 
-            _subscriber.Subscribe(messageType, m => notificationCallback(m.Headers.OfType<BusHeader>()), filter: filterHeaders);
+            _subscriber.Subscribe(messageType, m => notificationCallback(m.Headers.OfType<BusHeader>()), filter: filterHeaders, hierarchy: hierarchy);
         }
 
         public void Dispose()
