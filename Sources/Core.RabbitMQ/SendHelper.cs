566fb7c842eb0059f9f8eef5e0a1e818364f65d3
Savencov Maxim <msavencov@starlab.md>
2016-12-07 13:08:10 +0200

    Renamed project Core2 to Core.RabbitMQ

---
+++ Sources/Core.RabbitMQ/SendHelper.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using MessageBus.Core.API;
+using RabbitMQ.Client.Framing;
+
+namespace MessageBus.Core
+{
+    public class SendHelper : ISendHelper
+    {
+        private readonly ConcurrentDictionary<Type, DataContractKey> _nameMappings = new ConcurrentDictionary<Type, DataContractKey>();
+        
+        public void Send(SendParams sendParams)
+        {
+            sendParams.BusMessage.Sent = DateTime.Now;
+            sendParams.BusMessage.BusId = sendParams.BusId;
+
+            BasicProperties basicProperties = new BasicProperties
+            {
+                AppId = sendParams.BusMessage.BusId,
+                Timestamp = sendParams.BusMessage.Sent.ToAmqpTimestamp(),
+                ContentType = sendParams.Serializer.ContentType,
+                Headers = new Dictionary<string, object>()
+            };
+
+            byte[] bytes;
+            
+            if (sendParams.BusMessage.Data != null)
+            {
+                Type type = sendParams.BusMessage.Data.GetType();
+                DataContractKey contractKey = _nameMappings.GetOrAdd(type, t => t.GetDataContractKey());
+
+                basicProperties.Type = contractKey.Name;
+                basicProperties.Headers.Add(MessagingConstants.HeaderNames.Name, contractKey.Name);
+                basicProperties.Headers.Add(MessagingConstants.HeaderNames.NameSpace, contractKey.Ns);
+
+                sendParams.BusMessage.Name = contractKey.Name;
+                sendParams.BusMessage.Namespace = contractKey.Ns;
+
+                bytes = sendParams.Serializer.Serialize(sendParams.BusMessage);
+            }
+            else
+            {
+                bytes = new byte[0];
+            }
+            
+            foreach (BusHeaderBase header in sendParams.BusMessage.Headers)
+            {
+                basicProperties.Headers.Add(header.Name, header.GetValue());
+            }
+
+            if (sendParams.PersistentDelivery)
+            {
+                basicProperties.SetPersistent(true);
+            }
+
+            if (!string.IsNullOrEmpty(sendParams.ReplyTo))
+            {
+                basicProperties.ReplyTo = sendParams.ReplyTo;
+            }
+
+            if (!string.IsNullOrEmpty(sendParams.CorrelationId))
+            {
+                basicProperties.CorrelationId = sendParams.CorrelationId;
+            }
+
+            if (sendParams.Priority.HasValue)
+            {
+                basicProperties.Priority = sendParams.Priority.Value;
+            }
+
+            sendParams.Model.BasicPublish(sendParams.Exchange, sendParams.RoutingKey, sendParams.MandatoryDelivery, basicProperties, bytes);
+        }
+
+        public Type GetDataType(DataContractKey dataContractKey)
+        {
+            return _nameMappings.Where(pair => pair.Value.Equals(dataContractKey)).Select(pair => pair.Key).FirstOrDefault();
+        }
+    }
+}
---
+++ Sources/Core.RabbitMQ/SubscriberBase.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using MessageBus.Core.API;
+using RabbitMQ.Client;
+
+namespace MessageBus.Core
+{
+    public class SubscriberBase : ISubscription
+    {
+        private readonly IModel _model;
+        private readonly string _queue;
+        private readonly IBasicConsumer _consumer;
+        private readonly SubscriberConfigurator _configurator;
+
+        private string _consumerTag;
+        
+        public SubscriberBase(IModel model, string queue, IBasicConsumer consumer, SubscriberConfigurator configurator)
+        {
+            _model = model;
+
+            _queue = queue;
+
+            _consumer = consumer;
+            _configurator = configurator;
+        }
+
+        public void Dispose()
+        {
+            _model.Close();
+        }
+
+        public void Open()
+        {
+            if (_configurator.Prefetch > 0)
+            {
+                _model.BasicQos(0, _configurator.Prefetch, false);
+            }
+
+            _consumerTag = _model.BasicConsume(_queue, !_configurator.TransactionalDelivery, _configurator.ConsumerTag, !_configurator.ReceiveSelfPublish, false, new Dictionary<string, object>(), _consumer);
+        }
+
+        public void Close()
+        {
+            _model.BasicCancel(_consumerTag);
+        }
+    }
+}
